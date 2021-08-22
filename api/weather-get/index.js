236af1a7d9e9aa9f829456be0547153e94a2a4ee
Lukas Lundin <lukasedv@outlook.com>
2021-08-22 19:30:54 +0300

    api update

--- api/weather-get/index.js
+++ api/weather-get/index.js
@@ -2,12 +2,23 @@ const axios = require('axios');
 const baseUrl = 'https://aiwear-apim.azure-api.net/api/weather';
 
 
-module.exports = async function (context, req) {
-  const secretkey = process.env["REACT_APP_WEATHER_KEY"]
+module.exports = async function(context, req) {
+  context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
+  const config = {
+    headers: {'Ocp-Apim-Subscription-Key': process.env.WEATHER_KEY}
+  }
 
-  console.log(req.body)
-
-  context.res.json({
-      text: secretkey
-  });
+  try {
+    const response = await axios.get(`${baseUrl}/weather?lat=${req.body.lat}&lon=${req.body.lng}&units=metric&lang=en&mode=json`, config)
+    context.res = {
+      // status defaults to 200 */
+      body: response.data
+    };
+  } catch (err) {
+    context.res = {
+      status: 400,
+      body: err
+    };
+    console.error(err);
+  }
 };
